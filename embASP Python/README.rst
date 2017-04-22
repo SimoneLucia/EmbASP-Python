2d1c9bf3ec2e72ea030080892961444278072f20
SimoneLucia <luciasimone1994@gmail.com>
2017-04-22 17:41:57 +0200

    Aggiunti file README e LICENSE ed aggiornato setup.py

---
+++ embASP Python/README.rst
@@ -0,0 +1,5 @@
+EmbASP
+=======================
+
+EmbASP is a framework for the integration (embedding) of Logic Programming in external systems for generic applications.
+It helps developers at designing and implementing complex reasoning tasks by means of solvers on different platforms.
--- embASP Python/setup.py
+++ embASP Python/setup.py
@@ -2,16 +2,49 @@
 
 from distutils.core import setup
 from setuptools import find_packages
-import sys
+
+from codecs import open
+from os import path
+
+# import sys
 
 # if sys.version_info < (3,6):
 #     sys.exit('Sorry, Python < 3,6 is not supported')
-    
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='embASP',
-      version='0.1',
+      version='0.2',
       description='embASP',
+      long_description=long_description,
       author='Department of Mathematics and Computer Science, University of Calabria',
+      license='MIT',
       author_email='embasp@mat.unical.it',
       url='https://www.mat.unical.it/calimeri/projects/embasp/',
       packages=find_packages(),
+      
+      classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
+      
      )
+
+
+
+
+
