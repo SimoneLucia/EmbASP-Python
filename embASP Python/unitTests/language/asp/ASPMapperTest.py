3c981d8fa8f5899903b18ce27bfa846dcdb908ad
SimoneLucia <luciasimone1994@gmail.com>
2017-04-29 17:19:19 +0200

    Aggiunto unit test di ASPMapper

---
+++ embASP Python/unitTests/language/asp/ASPMapperTest.py
@@ -0,0 +1,39 @@
+import unittest
+from languages.asp.ASPMapper import ASPMapper
+from unitTests.specialization.dlv.Cell import Cell
+
+
+
+class ASPMapperTest(unittest.TestCase):
+
+    def test(self):
+        
+        instance = ASPMapper.getInstance()
+        
+        try:
+            instance.registerClass(Cell)
+            
+            object = instance.getObject("cell(1,2,5)")
+        
+            self.assertTrue(isinstance(object, Cell))
+            
+            self.assertEqual(1, object.getRow())
+            
+            self.assertEqual(2, object.getColumn())
+            
+            self.assertEqual(5, object.getValue())
+            
+            self.assertEqual("cell(1,2,5)", instance.getString(object))
+            
+            instance.unregisterClass(Cell)
+            
+            noneObject = instance.getObject("cell(1,2,5)")
+            
+            self.assertIsNone(noneObject)
+            
+        except Exception as e:
+            self.fail(e.message)
+
+
+if __name__ == '__main__':
+    unittest.main()
