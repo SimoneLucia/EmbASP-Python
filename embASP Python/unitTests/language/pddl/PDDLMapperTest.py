903e4398b60bf79f842aa03855a96198330ac754
SimoneLucia <luciasimone1994@gmail.com>
2017-04-29 17:21:12 +0200

    Aggiunto unit test di PDDLMapper

---
+++ embASP Python/unitTests/language/pddl/PDDLMapperTest.py
@@ -0,0 +1,26 @@
+import unittest
+from languages.pddl.PDDLMapper import PDDLMapper
+from unitTests.specialization.solver_planning_domains.PickUp import PickUp
+
+
+class PDDLMapperTest(unittest.TestCase):
+
+    def test(self):
+        
+        instance = PDDLMapper.getInstance()
+        
+        try:
+            instance.registerClass(PickUp)
+            
+            object = instance.getObject("(pick-up b)")
+        
+            self.assertTrue(isinstance(object, PickUp))
+            
+            self.assertEqual("b", object.getBlock())
+            
+        except Exception as e:
+            self.fail(e.message)
+
+
+if __name__ == '__main__':
+    unittest.main()
