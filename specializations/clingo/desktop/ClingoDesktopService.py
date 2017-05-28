4a92a4e013e49c73d4fb8c7a0d732988b0d9dc97
SimoneLucia <luciasimone1994@gmail.com>
2017-05-28 13:38:33 +0200

    Fix stdin bug Clingo
    Modificata stringa "_load_from_STDIN_option" in ClingoDesktopService,
    che non permetteva il corretto funzionamento dello standard input del
    processo clingo

--- specializations/clingo/desktop/ClingoDesktopService.py
+++ specializations/clingo/desktop/ClingoDesktopService.py
@@ -6,7 +6,7 @@ class ClingoDesktopService(DesktopService):
     
     def __init__(self, exe_path):
         super(ClingoDesktopService, self).__init__(exe_path)
-        self._load_from_STDIN_option = ""
+        self._load_from_STDIN_option = "-"
         
     def _getOutput(self, output, error):
         return ClingoAnswerSets(output, error)
