378f3c2b9f1363ed9b3b79d35024622efa8fb11b
SimoneLucia <luciasimone1994@gmail.com>
2017-06-12 17:22:30 +0200

    Modifiche al file setup.py
    - Modificato il nome del package degli unit test da escludere
    nell'istallazione
    - eliminati commenti non più utili
    - modificato lo stato di sviluppo nel classifiers
    - modificata versione

--- setup.py
+++ setup.py
@@ -6,32 +6,27 @@ from setuptools import find_packages
 from codecs import open
 from os import path
 
-# import sys
-
-# if sys.version_info < (3,6):
-#     sys.exit('Sorry, Python < 3,6 is not supported')
-
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='embASP',
-      version='0.4',
+      version='0.5',
       description='embASP',
       long_description=long_description,
       author='Department of Mathematics and Computer Science, University of Calabria',
       license='MIT',
       author_email='embasp@mat.unical.it',
       url='https://www.mat.unical.it/calimeri/projects/embasp/',
-      packages=find_packages(exclude=["*.unitTests", "*.unitTests.*", "unitTests.*", "unitTests"]),
+      packages=find_packages(exclude=["*.test", "*.test.*", "test.*", "test"]),
       
       classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
